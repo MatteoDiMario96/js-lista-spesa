33e426ae260ba851492ad29dc38df6389e78ac65
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-15 15:40:33 +0200

    Creato ciclo while che va da indice 0 a lunghezza dell'array - 1.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 const ulAppend = document.querySelector("ul");
 
 const listaSpesa = ["Pane", "Affettati vari", "Verdure varie", "Parmigiano", "Snack salutari", "Formaggio", "Latte", "Yogurt", "Biscotti", "Dolci Vari", "Pesto", "Succhi di frutta", "Gelato", "Pizza surgelata"];
+
+let i = 0;
+
+while(i < listaSpesa.length - 1){
+    i++
+    
+}
