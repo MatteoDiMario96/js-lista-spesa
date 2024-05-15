9d8e2e53d5b1fbd686890ccd09ff41abcb8f6169
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-15 15:27:42 +0200

    Creata ul nell'HTML. Selezionata ul nel js, creato array con lista della spesa varia.

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const ulAppend = document.querySelector("ul");
+
+const listaSpesa = ["Pane", "Affettati vari", "Verdure varie", "Parmigiano", "Snack salutari", "Formaggio", "Latte", "Yogurt", "Biscotti", "Dolci Vari", "Pesto", "Succhi di frutta", "Gelato", "Pizza surgelata"];
